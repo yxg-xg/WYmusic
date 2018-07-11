ef22b56ff91c9aa6cca9009cc6a751ecee05b76b
yxg-xg <923218778@qq.com>
2018-07-11 14:31:50 +0800

    wy

--- front_end/src/components/song/song.vue
+++ front_end/src/components/song/song.vue
@@ -4,7 +4,26 @@
     <div class="indistinct"></div>
     <div class="songMain">
       <div class="wyLogo"></div>
+      <div class="opticalDiskWrap">
+        <div class="opticalDisk ">
+          <transition name="a-circling">
+            <div class="songImg" :class="{'a-circling':isCircling}">
+              <img :src=songImg alt=""/>
+            </div>
+          </transition>
 
+          <transition name="a-circling">
+            <div class="song-light" :class="{'a-circling':isCircling}"></div>
+          </transition>
+
+
+          <div class="m-start" v-show="startShow">
+
+          </div>
+        </div>
+        <div class="m-song-clickarea" @click="clickarea"></div>
+      </div>
+      <div class="broadcaster"></div>
 
     </div>
   </div>
@@ -15,15 +34,24 @@
     name: 'song',
     data(){
       return{
+        songImg:require("@/assets/img/songBg/thatGirl.webp"),
+        startShow:true,
+        isCircling:true,
 
       }
     },
-    methods:{},
+    methods:{
+      clickarea(){
+        this.startShow = !this.startShow;
+        this.isCircling = !this.isCircling;
+        console.log()
+      }
+    },
     created:function(){
       console.log('song')
     }
   }
 </script>
-<style lang="css" scoped>
-  @import "song.css"
+<style lang="less" scoped src="./song.less">
+  //@import "song.css"
 </style>
